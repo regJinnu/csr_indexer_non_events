d8b00355aa564c83fd2c5c3cf4be4f85ef6d12f5
shivanimalviya <shivani.malviya@gdn-commerce.com>
2019-12-13 11:30:24 +0530

    PR comments resolved for tradeIn event

--- src/test/java/com/gdn/qa/x_search/api/test/steps/TradeInAggregateEventSteps.java
+++ src/test/java/com/gdn/qa/x_search/api/test/steps/TradeInAggregateEventSteps.java
@@ -5,6 +5,7 @@ import com.gdn.qa.x_search.api.test.data.SearchServiceData;
 import com.gdn.qa.x_search.api.test.properties.SearchServiceProperties;
 import com.gdn.qa.x_search.api.test.utils.KafkaHelper;
 import com.gdn.qa.x_search.api.test.utils.SolrHelper;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -30,33 +31,35 @@ public class TradeInAggregateEventSteps {
   @Autowired
   KafkaHelper kafkaHelper;
 
-  @Given("^\\[search-service] prepare request for processing trade in aggregate event$")
-  public void searchServicePrepareRequestForProcessingTradeInAggregateEvent() {
+  @Given("^\\[search-service] prepare request for processing trade in aggregate (.*) event$")
+  public void searchServicePrepareRequestForProcessingTradeInAggregateEvent(String active) {
     searchServiceData.setId(searchServiceProperties.get("idForTradeInEvent"));
     searchServiceData.setProductSku(searchServiceProperties.get("idForTradeInEvent"));
     searchServiceData.setProductName(searchServiceProperties.get("productNameForTradeInEvent"));
-    searchServiceData.setActive(Boolean.parseBoolean("true"));
+    searchServiceData.setActive(Boolean.parseBoolean(active));
+  }
+
+  @And("^\\[search-service] set trade in value as (.*) of test product based on (.*)$")
+  public void searchServiceSetTradeInFieldOfTestProduct(String preEventTradeInValue, String caseToBeUpdated) throws Exception {
+    String query = searchServiceProperties.get("solrQueryForTradeInEvent");
+    int status = solrHelper.updateSolrDataForAutomation(query,
+        SELECT_HANDLER,
+        "id",
+        1,
+        caseToBeUpdated,
+        SOLR_DEFAULT_COLLECTION);
+    assertThat("Updating tradeInEligible in SOLR doc failed", status, equalTo(0));
+    solrHelper.solrCommit(SOLR_DEFAULT_COLLECTION);
+    boolean tradeInEligible =
+        solrHelper.getSolrProd(query, SELECT_HANDLER, "tradeInEligible", 1, SOLR_DEFAULT_COLLECTION)
+            .get(0)
+            .getTradeInEligible();
+
+    assertThat("Test Product not set in SOLR", tradeInEligible, equalTo(Boolean.parseBoolean(preEventTradeInValue)));
   }
 
   @When("^\\[search-service] send request for processing trade in aggregate event$")
   public void searchServiceSendRequestForProcessingTradeInAggregateEvent() throws Exception {
-
-    String query = searchServiceProperties.get("solrQueryForTradeInEvent");
-    int status = solrHelper.updateSolrDataForAutomation(query,
-        SELECT_HANDLER,
-        "id",
-        1,
-        "tradeInEligible",
-        SOLR_DEFAULT_COLLECTION);
-    assertThat("Updating tradeInEligible in SOLR doc failed", status, equalTo(0));
-    solrHelper.solrCommit(SOLR_DEFAULT_COLLECTION);
-    boolean tradeInEligible =
-        solrHelper.getSolrProd(query, SELECT_HANDLER, "tradeInEligible", 1, SOLR_DEFAULT_COLLECTION)
-            .get(0)
-            .getTradeInEligible();
-
-    assertThat("Test Product not set in SOLR", tradeInEligible, equalTo(false));
-
     kafkaHelper.publishTradeInAggregateEvent(searchServiceData.getId(),
         searchServiceData.getProductSku(),
         searchServiceData.getProductName(),
@@ -66,61 +69,14 @@ public class TradeInAggregateEventSteps {
   }
 
 
-  @Then("^\\[search-service] check if trade in aggregate event is processed and solr is updated$")
-  public void searchServiceCheckIfTradeInAggregateEventIsProcessedAndSolrIsUpdated()
+  @Then("^\\[search-service] check if trade in aggregate event is processed and solr is updated with (.*) as value$")
+  public void searchServiceCheckIfTradeInAggregateEventIsProcessedAndSolrIsUpdated(String active)
       throws Exception {
     String query = searchServiceProperties.get("solrQueryForTradeInEvent");
     boolean tradeInEligible =
         solrHelper.getSolrProd(query, SELECT_HANDLER, "tradeInEligible", 1, SOLR_DEFAULT_COLLECTION)
             .get(0)
             .getTradeInEligible();
-    assertThat("Test Product not updated after event processing", tradeInEligible, equalTo(true));
-  }
-
-  @Given("^\\[search-service] prepare request for processing tradeIn aggregate inEligible event$")
-  public void searchServicePrepareRequestForProcessingTradeInAggregateInEligibleEvent() {
-    searchServiceData.setId(searchServiceProperties.get("idForIneligibleTradeInEvent"));
-    searchServiceData.setProductSku(searchServiceProperties.get("idForIneligibleTradeInEvent"));
-    searchServiceData.setProductName(searchServiceProperties.get(
-        "productNameForIneligibleTradeInEvent"));
-    searchServiceData.setActive(Boolean.parseBoolean("false"));
-  }
-
-  @When("^\\[search-service] send request for processing tradeIn aggregate inEligible event$")
-  public void searchServiceSendRequestForProcessingTradeInAggregateInEligibleEvent()
-      throws Exception {
-    String query = searchServiceProperties.get("solrQueryForIneligibleTradeInEvent");
-    int status = solrHelper.updateSolrDataForAutomation(query,
-        SELECT_HANDLER,
-        "id",
-        1,
-        "tradeInInEligible",
-        SOLR_DEFAULT_COLLECTION);
-    assertThat("Updating tradeInEligible in SOLR doc failed", status, equalTo(0));
-    solrHelper.solrCommit(SOLR_DEFAULT_COLLECTION);
-    boolean tradeInEligible =
-        solrHelper.getSolrProd(query, SELECT_HANDLER, "tradeInEligible", 1, SOLR_DEFAULT_COLLECTION)
-            .get(0)
-            .getTradeInEligible();
-
-    assertThat("Test Product not set in SOLR", tradeInEligible, equalTo(true));
-
-    kafkaHelper.publishTradeInAggregateEvent(searchServiceData.getId(),
-        searchServiceData.getProductSku(),
-        searchServiceData.getProductName(),
-        searchServiceData.isActive());
-    Thread.sleep(30000);
-    solrHelper.solrCommit(SOLR_DEFAULT_COLLECTION);
-  }
-
-  @Then("^\\[search-service] check if tradeIn aggregate inEligible event is processed and solr is updated$")
-  public void searchServiceCheckIfTradeInAggregateInEligibleEventIsProcessedAndSolrIsUpdated()
-      throws Exception {
-    String query = searchServiceProperties.get("solrQueryForIneligibleTradeInEvent");
-    boolean tradeInEligible =
-        solrHelper.getSolrProd(query, SELECT_HANDLER, "tradeInEligible", 1, SOLR_DEFAULT_COLLECTION)
-            .get(0)
-            .getTradeInEligible();
-    assertThat("Test Product not updated after event processing", tradeInEligible, equalTo(false));
+    assertThat("Test Product not updated after event processing", tradeInEligible, equalTo(Boolean.parseBoolean(active)));
   }
 }
--- src/test/resources/features/TradeInAggregateEvent.feature
+++ src/test/resources/features/TradeInAggregateEvent.feature
@@ -1,14 +1,13 @@
-@TradeInAggregateEvent @TestSuiteID=11050177
+@TradeInAggregateEventFeature @TestSuiteID=11050177
 Feature: Trade In Aggregate event feature
 
-  @TradeInAggregateEventProcessing @Regression
-  Scenario: Verify trade in aggregate event processing
-    Given [search-service] prepare request for processing trade in aggregate event
+  @TradeInAggregateEventProcessing @Regression @Positive
+  Scenario Outline: Verify trade in aggregate event processing
+    Given [search-service] prepare request for processing trade in aggregate <active> event
+    And [search-service] set trade in value as <preEventTradeInValue> of test product based on <eligibility>
     When [search-service] send request for processing trade in aggregate event
-    Then [search-service] check if trade in aggregate event is processed and solr is updated
-
-  @TradeInAggregateInEligibleEventProcessing @Regression
-  Scenario: Verify tradeIn aggregate inEligible event processing
-    Given [search-service] prepare request for processing tradeIn aggregate inEligible event
-    When [search-service] send request for processing tradeIn aggregate inEligible event
-    Then [search-service] check if tradeIn aggregate inEligible event is processed and solr is updated
+    Then [search-service] check if trade in aggregate event is processed and solr is updated with <active> as value
+    Examples:
+      | active | preEventTradeInValue | eligibility       |
+      | true   | false                | tradeInEligible   |
+      | false  | true                 | tradeInInEligible |
